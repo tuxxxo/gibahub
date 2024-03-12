d350d0d3429bf23f2fea28cd7d4d187100384812
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-03-12 08:45:58 -0300

    asd

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const botoes = document.querySelectorAll(".botao"); 
-for(let i = 0 <botoes.length; j++)
+for(let i = 0 <botoes.length; j++){
+    botoes[i].onclick = function(){
+        for (let k = 0; < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+    }
+}
