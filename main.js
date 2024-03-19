939e34bfd1a7e2821fb2ca9f5387abff5f7da39f
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-03-19 07:58:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao"); 
-for(let i = 0 <botoes.length; j++){
+for(let i = 0 <botoes.length; i++){
     botoes[i].onclick = function(){
         for (let k = 0; j< botoes.length; j++){
             botoes[j].classList.remove("ativo");
