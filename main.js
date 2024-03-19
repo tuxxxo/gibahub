a368104d256edb2d4803ece933236f341af389e0
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-03-19 08:08:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao"); 
-for(let i = 0 <botoes.length; i++){
+for(let i = 0; i <botoes.length; i++){
     botoes[i].onclick = function(){
-        for (let k = 0; j< botoes.length; j++){
+        for (let j = 0; j< botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
