4ce4956907a79ac39bde5606c3ef203fdefe86d4
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-04-02 08:18:06 -0300

    asd

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ for(let i = 0; i <botoes.length; i++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo")
+
+        botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
