9379f12e06a04745873371844793ec07cb274190
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-04-30 08:12:54 -0300

    asd

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ for(let i = 0; i <botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll (".contador");
-const tempoObjetivo1 = new Date ("2024-12-22");
+const tempoObjetivo1 = new Date ("2024-12-22T23:59:59");
 
 const tempos = [tempoObjetivo1]
 
