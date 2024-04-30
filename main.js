7f701202293e1f36e0dfc6e0bd9e11e476ec84cf
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-04-30 07:40:07 -0300

    asd

--- main.js
+++ main.js
@@ -25,7 +25,10 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal =- tempoObjetivo1 - tempoAtual
-    
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor (horas / 24);
 }
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
