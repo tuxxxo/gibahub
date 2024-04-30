fab3f24dd9ad82ba0e8842837d1b15ab154a418c
tuxxxo <105920026+tuxxxo@users.noreply.github.com>
2024-04-30 07:37:34 -0300

    cas

--- main.js
+++ main.js
@@ -16,4 +16,16 @@ const contadores = document.querySelectorAll (".contador");
 const tempoObjetivo1 = new Date ("2024-12-22");
 let tempoAtual = new Date();
 
+for (let i=0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal =- tempoObjetivo1 - tempoAtual
+    
+}
+
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
